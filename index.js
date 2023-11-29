d26616d67fbcf5ff922092ec2b848bd550c66278
tclar <tclar001@campus.goldsmiths.ac.uk>
2023-11-29 12:24:48 +0000

    Lab 4 forum-app dynamic web. basic home page added

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
- 
+ // Import the modules we need
+var express = require ('express')
+var ejs = require('ejs')
+var mysql = require('mysql');
+var bodyParser= require ('body-parser')
+
+// Create the express application object
+const app = express()
+const port = 8000
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set up css
+app.use(express.static(__dirname + '/public'));
+
+// Directory where Express will pick up HTML files
+// __dirname will get the current directory
+app.set('views', __dirname + '/views');
+
+// Tell Express that we want to use EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tells Express how we should process html files
+// We want to use EJS's rendering engine
+app.engine('html', ejs.renderFile);
+
+// Define our data
+var forumData = {forumName: "Critically Acclaimed Forum"}
+
+// Requires the main.js file inside the routes folder passing in the Express app and data as arguments.  All the routes will go in this file
+require("./routes/routes")(app, forumData);
+
+// Start the web app listening
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ routes/routes.js
@@ -0,0 +1,8 @@
+module.exports = function(app, forumData) {
+
+    // Handle our routes
+    app.get('/',function(req,res){
+        res.render('index.ejs', forumData)
+    });
+
+}
